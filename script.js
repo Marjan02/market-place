623bfcfb8e6c200e8201f6b66d1ea8592b184962
Nivth <kevinzevalja@gmail.com>
2022-07-20 18:12:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const subtraction = document.getElementById("subtraction");
+const increase = document.getElementById("increase");
+const numberOfProducts = document.getElementById("number-of-products");
+
+function increaseNumber() {
+  
+}
+
+function substractNumber() {
+  numberOfProducts.value = parseInt(numberOfProducts.value) - 1;
+}
+
+console.log("ahok")
